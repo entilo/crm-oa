4c8496295cd5b197c5ff5ba3e049affc73e37b21
1245193880 <1245193880@qq.com>
2017-02-14 23:23:22 +0800

    update

--- src/main/java/com/crm/www/controller/LoginAndSignController.java
+++ src/main/java/com/crm/www/controller/LoginAndSignController.java
@@ -1,5 +1,24 @@
 package com.crm.www.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/home")
+@Controller
 public class LoginAndSignController {
 
+	@RequestMapping("/login")
+	public String login() {
+		return null;
+	}
+
+	@RequestMapping("/loginOut")
+	public String loginOut() {
+		return null;
+	}
+
+	@RequestMapping("/sign")
+	public String sign() {
+		return null;
+	}
 }
