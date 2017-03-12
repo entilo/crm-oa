4d9b393a3ed9073e9490ee9462ad0d92f95148e9
1245193880 <1245193880@qq.com>
2017-03-12 18:18:47 +0800

    update

--- src/main/java/com/crm/www/entity/Notice.java
+++ src/main/java/com/crm/www/entity/Notice.java
@@ -3,53 +3,61 @@ package com.crm.www.entity;
 import java.util.Date;
 
 public class Notice {
-    private Integer noticeId;
+	private Integer noticeId;
 
-    private Integer noticeFromId;
+	private Integer noticeFromId;
 
-    private String noticeToIds;
+	private String noticeToIds;
 
-    private Date noticeStartTime;
+	private Date noticeStartTime;
 
-    private String noticeContent;
+	private String noticeContent;
 
-    public Integer getNoticeId() {
-        return noticeId;
-    }
+	public Integer getNoticeId() {
+		return noticeId;
+	}
 
-    public void setNoticeId(Integer noticeId) {
-        this.noticeId = noticeId;
-    }
+	public void setNoticeId(Integer noticeId) {
+		this.noticeId = noticeId;
+	}
 
-    public Integer getNoticeFromId() {
-        return noticeFromId;
-    }
+	public Integer getNoticeFromId() {
+		return noticeFromId;
+	}
 
-    public void setNoticeFromId(Integer noticeFromId) {
-        this.noticeFromId = noticeFromId;
-    }
+	public void setNoticeFromId(Integer noticeFromId) {
+		this.noticeFromId = noticeFromId;
+	}
 
-    public String getNoticeToIds() {
-        return noticeToIds;
-    }
+	public String getNoticeToIds() {
+		return noticeToIds;
+	}
 
-    public void setNoticeToIds(String noticeToIds) {
-        this.noticeToIds = noticeToIds;
-    }
+	public void setNoticeToIds(String noticeToIds) {
+		this.noticeToIds = noticeToIds;
+	}
 
-    public Date getNoticeStartTime() {
-        return noticeStartTime;
-    }
+	public Date getNoticeStartTime() {
+		return noticeStartTime;
+	}
 
-    public void setNoticeStartTime(Date noticeStartTime) {
-        this.noticeStartTime = noticeStartTime;
-    }
+	public void setNoticeStartTime(Date noticeStartTime) {
+		this.noticeStartTime = noticeStartTime;
+	}
 
-    public String getNoticeContent() {
-        return noticeContent;
-    }
+	public String getNoticeContent() {
+		return noticeContent;
+	}
 
-    public void setNoticeContent(String noticeContent) {
-        this.noticeContent = noticeContent;
-    }
+	public void setNoticeContent(String noticeContent) {
+		this.noticeContent = noticeContent;
+	}
+
+	@Override
+	public String toString() {
+		return "Notice [noticeId=" + noticeId + ", noticeFromId=" + noticeFromId
+				+ ", noticeToIds=" + noticeToIds + ", noticeStartTime="
+				+ noticeStartTime + ", noticeContent=" + noticeContent + "]";
+	}
+
 }
---
+++ src/test/java/com/crm/www/crm/other/dao/NoticeMapperTest.java
@@ -0,0 +1,45 @@
+package com.crm.www.crm.other.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-dao.xml")
+public class NoticeMapperTest {
+@Autowired
+private NoticeMapper noticeMapper;
+	@Test
+	public void testDeleteByPrimaryKey() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertSelective() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByPrimaryKey() {
+		System.out.println(noticeMapper.selectByPrimaryKey(1));
+	}
+
+	@Test
+	public void testUpdateByPrimaryKeySelective() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateByPrimaryKey() {
+		fail("Not yet implemented");
+	}
+
+}
